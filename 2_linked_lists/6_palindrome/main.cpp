cf8e31c3b550964156cb3cb38963eadb7bd013b0
André Moreira Souza <msouza.andre@hotmail.com>
2019-12-12 12:40:17 +0200

    Implemented 2.6

---
+++ 2_linked_lists/6_palindrome/main.cpp
@@ -0,0 +1,66 @@
+// Implement a function to check if a linked list is a palindrome
+
+// * Solution time complexity: O(n)
+// * Solution space complexity: O(n)
+
+#include <forward_list>
+#include <iostream>
+#include <stack>
+
+typedef unsigned char uchar;
+
+using namespace std;
+
+template <typename T>
+bool isPalindrome(forward_list<T> t_list) {
+  // using an iterative approach with a stack
+  stack<T> st;
+  auto cur = t_list.begin(), cmp = t_list.begin();
+
+  // push first half of the list into a stack
+
+  while (cmp != t_list.end() && next(cmp) != t_list.end()) {
+    st.push(*cur);
+    cur = next(cur);
+    cmp = next(next(cmp));
+  }
+
+  // solving odd number of elements
+  if (cmp != t_list.end()) {
+    cur = next(cur);
+  }
+
+  // popping from stack and comparing to second half of the list
+  while (cur != t_list.end()) {
+    T top = st.top();
+    st.pop();
+
+    if (top != *cur) return false;
+    cur = next(cur);
+  }
+  return true;
+}
+
+// int main(int argc, char **argv) { // not using argc/argv
+int main(void) {
+  forward_list<int> list;
+  auto it = list.before_begin();
+  int val;
+
+  // receiving list from stdin
+  // inserting sequentially in list
+  while (cin >> val) {
+    it = list.insert_after(it, val);
+  }
+
+  if (list.empty()) {
+    cout << "List is empty. Aborting..." << endl;
+    return 0;
+  }
+
+  bool is_palindrome = isPalindrome(list);
+
+  cout << is_palindrome << endl;
+
+  return 0;
+}
