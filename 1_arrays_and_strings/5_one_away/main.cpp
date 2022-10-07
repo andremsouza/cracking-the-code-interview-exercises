eb0497a6339e4b8952867a3b00e672c720234f64
André Moreira Souza <msouza.andre@hotmail.com>
2022-10-07 16:25:09 +0000

    Add test files to 5_one_away. Modify output

--- 1_arrays_and_strings/5_one_away/main.cpp
+++ 1_arrays_and_strings/5_one_away/main.cpp
@@ -8,98 +8,60 @@
 // pale, bake -> false
 // ple, pale
 
-// * Solution time complexity: O(n), n -> length of shorter string
-// * Solution space complexity: O(1)
-
 #include <iostream>
 #include <string>
 
-using namespace std;
-
-bool isReplace(string t_a, string t_b) {
-  bool one_replace = false;
-  // checking length of each string
-  if (t_b.length() == t_a.length()) {
-    // comparing character by character
-    for (int i = 0; i < (int)t_a.length(); i++) {
-      // if character is different, check one_replace flag
-      if (t_a[i] != t_b[i]) {
-        if (one_replace) {
-          // if a replacement was already detected before, return false
-          return false;
-        } else {
-          one_replace = true;
-        }
+bool IsReplaceChar(std::string str1, std::string str2) {
+  auto isReplaceChar = false;
+  if (str1.length() != str2.length()) return false;
+  for (int i = 0; i < str1.length(); i++) {
+    if (str1[i] != str2[i]) {
+      if (isReplaceChar) {
+        return false;
       }
+      isReplaceChar = true;
     }
-    return one_replace;
-  } else {
-    // if invalid length, return false
-    return false;
   }
+  return true;
 }
 
-bool isAddition(string t_a, string t_b) {
-  bool one_addition = false;
-  int i = 0, j = 0;
-  // checking length of each string
-  if (t_b.length() == t_a.length() + 1) {
-    // comparing character by character
-    while (i < (int)t_a.length() && j < (int)t_b.length()) {
-      // if char is different, advance on second string and set flag
-      if (t_a[i] != t_b[j]) {
-        j++;
-        if (one_addition) {
-          // if addition already detected, return false
-          return false;
-        } else {
-          one_addition = true;
-        }
-      } else {
-        i++, j++;
+bool IsInsertChar(std::string str1, std::string str2) {
+  auto index1 = 0, index2 = 0;
+  if (str1.length() + 1 != str2.length()) return false;
+  while (index1 < str1.length() && index2 < str2.length()) {
+    if (str1[index1] != str2[index2]) {
+      if (index1 != index2) {
+        return false;
       }
+      index2++;
+    } else {
+      index1++;
+      index2++;
     }
-    // if the insertion is at the end of t_a
-    if (i == (int)t_a.length()) {
-      one_addition = true;
-    }
-    return one_addition;
-  } else {
-    // invalid length -> not an addition
-    return false;
   }
+  return true;
 }
 
-bool isOneEdit(string t_a, string t_b) {
-  // if strings are equal, return true
-  if (!t_a.compare(t_b)) {
-    return true;
-  }
-  if (t_b.length() == t_a.length()) {
-    // checking replacement
-    return isReplace(t_a, t_b);
-  } else if (t_b.length() == t_a.length() + 1) {
-    // checking addition
-    return isAddition(t_a, t_b);
-  } else if (t_b.length() == t_a.length() - 1) {
-    // checking remove
-    return isAddition(t_b, t_a);
-  } else {
-    // base case
-    return false;
-  }
+bool IsOneEditAway(std::string str1, std::string str2) {
+  if (str1 == str2) return true;  // if strings are equal, return true
+  if (str1.length() == str2.length())
+    return IsReplaceChar(str1, str2);
+  else if (str1.length() + 1 == str2.length())
+    return IsInsertChar(str1, str2);
+  else if (str1.length() - 1 == str2.length())
+    return IsInsertChar(str2, str1);
+  return false;  // if strings are not equal and not one edit away, return false
 }
 
-// int main(int argc, char **argv) { // not using argc/argv
+// int main(int argc, char *argv[]) { // not using argc and argv for now
 int main(void) {
-  string a, b;
+  std::string str1, str2;
 
-  // reading string from stdin
-  getline(cin, a);
-  getline(cin, b);
+  // read input strings from stdin
+  std::cin >> str1 >> str2;
 
-  // checking and writing output to stdout
-  cout << "isOneEdit = " << isOneEdit(a, b) << endl;
+  // print result
+  std::cout << IsOneEditAway(str1, str2) << std::endl;
 
   return 0;
 }
