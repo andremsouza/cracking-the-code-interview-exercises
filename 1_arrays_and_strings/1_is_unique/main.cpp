9f3e23f5259e20b190511949efb0b709279e146f
André Moreira Souza <msouza.andre@hotmail.com>
2022-10-07 20:28:57 +0000

    Modify 1_is_unique to receive command line arguments

--- 1_arrays_and_strings/1_is_unique/main.cpp
+++ 1_arrays_and_strings/1_is_unique/main.cpp
@@ -5,26 +5,40 @@
 // Solution complexity: O(n) time, O(1) space
 
 #include <array>
+#include <cstring>
 #include <iostream>
 #include <string>
 
 // #define ASCII_SIZE 256
 constexpr ushort kAsciiSize = 256;
 
-// int main(int argc, char const *argv[]) { // not using argc/argv
-int main() {
+// int main() {
+int main(int argc, char const *argv[]) {
   std::array<bool, kAsciiSize> charset = {
       false};                 // bit vector initialized with zeros
   auto is_all_unique = true;  // output flag
   unsigned char c;            // input character
 
-  // read input from stdin and check bitvector
-  while (std::cin >> c) {
-    if (charset[c]) {
-      is_all_unique = false;
-      break;
+  // Check if parameter is passed
+  if (argc > 1) {
+    // read input parameter and check bit vector
+    for (auto i = 0; i < (int)std::strlen(argv[1]); i++) {
+      c = argv[1][i];
+      if (charset[c]) {
+        is_all_unique = false;
+        break;
+      }
+      charset[c] = true;
     }
-    charset[c] = true;
+  } else {
+    // read input from stdin and check bitvector
+    while (std::cin >> c) {
+      if (charset[c]) {
+        is_all_unique = false;
+        break;
+      }
+      charset[c] = true;
+    }
   }
 
   // Print output to stdout
