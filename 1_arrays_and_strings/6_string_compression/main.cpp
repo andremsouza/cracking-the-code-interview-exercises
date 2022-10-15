a8a1f40e0b287aa53214e87537fc0610f8468f15
André Moreira Souza <msouza.andre@hotmail.com>
2022-10-15 20:48:22 +0000

    Convert code to use smart pointers. Add unit tests

--- 1_arrays_and_strings/6_string_compression/main.cpp
+++ 1_arrays_and_strings/6_string_compression/main.cpp
@@ -10,40 +10,52 @@
 // *  will need 2 * n characters.
 
 #include <iostream>
+#include <memory>
 #include <string>
 
-using namespace std;
+/**
+ * @brief Compresses a string using the counts of repeated characters.
+ *
+ * @details The function iterates over the string and counts the number of
+ *  repeated characters. If the compressed string would not become smaller than
+ *  the original string, the function returns the original string.
+ *  Solution complexity: O(n) in time and O(n) in space.
+ *
+ * @param str The string to be compressed.
+ * @return The compressed string.
+ */
+std::string compressString(const std::string &str) {
+  if (!str.length()) return str;  // if string is empty, return it
+  auto cur = str.front();         // stores current character from string
+  auto cur_freq = 0;              // stores frequency of current character
+  auto ret = std::string();       // stores the resulting string
 
-string compressString(const string t_str) {
-  // if string is empty, return it
-  if (!t_str.length()) {
-    return t_str;
-  }
-  char cur = t_str.front();  // stores current character from string
-  int cur_freq = 0;          // stores frequency of current character
-  string ret;                // stores returning string
-
-  for (char const &c : t_str) {
+  for (const auto &c : str)
     // if character changes, write current character and count to ret string
     // then, begin counting the new character
     // else, increment count of current character
     if (c != cur) {
-      ret += cur + to_string(cur_freq);
+      ret += cur + std::to_string(cur_freq);
+      if (ret.length() >= str.length()) return str;
       cur = c;
       cur_freq = 1;
     } else {
       cur_freq++;
     }
-  }
-  ret += cur + to_string(cur_freq);
-  return ret.length() < t_str.length() ? ret : t_str;
+
+  ret += cur + std::to_string(cur_freq);
+  return ret.length() < str.length() ? ret : str;
 }
 
-// int main(int argc, char **argv) { // not using argc/argv
-int main(void) {
-  string a;
-  // getting string from stdin
-  getline(cin, a);
-  cout << compressString(a) << endl;
+int main(int argc, char **argv) {
+  auto str = std::make_unique<std::string>();
+
+  if (argc > 1)
+    *str = argv[1];
+  else
+    std::getline(std::cin, *str);
+
+  // print result
+  std::cout << compressString(*str) << std::endl;
   return 0;
 }
